a1200f5380bebc7e600843c5c9acfa6aa9b71fa8
InGyu Moon <ansdlsrb1121@naver.com>
2024-05-10 17:12:07 +0900

    develop branch 생성

--- src/main/java/org/example/kushousebackend/KusHouseBackendApplication.java
+++ src/main/java/org/example/kushousebackend/KusHouseBackendApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class KusHouseBackendApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(KusHouseBackendApplication.class, args);
 	}
