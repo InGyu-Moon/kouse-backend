2605b47b20202ba10b615e2763a29a36d943e561
InGyu Moon <ansdlsrb1121@naver.com>
2024-05-10 10:25:59 +0900

    feat: 프로젝트 생성

---
+++ src/main/java/org/example/kushousebackend/KusHouseBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.kushousebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KusHouseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KusHouseBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/kushousebackend/KusHouseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.kushousebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KusHouseBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
