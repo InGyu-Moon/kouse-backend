5db588e9036c5c81a74c961171a7646709b4940a
InGyu Moon <ansdlsrb1121@naver.com>
2024-05-10 17:26:54 +0900

    refactor: JoinController 이름 변경

--- src/main/java/org/example/kushousebackend/controller/JoinController.java
+++ src/main/java/org/example/kushousebackend/controller/JoinController.java
@@ -7,13 +7,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1")
 @RequiredArgsConstructor
-public class MemberController {
+public class JoinController {
 
     private final MemberService memberService;
 
